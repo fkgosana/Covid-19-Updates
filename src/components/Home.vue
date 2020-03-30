681c1e60052f700a19230371d09a29279ec9ee4c
Frans <fkgosana48@gmail.com>
2020-03-30 14:08:40 +0200

    delete helloworld and add home component

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <button @click="test()">Click</button>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'Home',
+    data(){
+        return {
+            values : {},
+            error : []
+        }
+    },
+    mounted(){
+        axios.get('https://apicovid19.com/summary')
+        .then( response => {
+            this.values = response.data
+        })
+    },
+    methods: {
+        test(){
+            console.log(this.values)
+            alert('Testing')
+        }
+    }
+}
+</script>
