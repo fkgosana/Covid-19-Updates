c7ed787929c42c7c650140e2b24d1548d739ed76
Frans <fkgosana48@gmail.com>
2020-03-30 22:14:40 +0200

    use axios to get coutries

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
 <div>
-    <button @click="test()">Click</button>
+    <ul v-for="(item,index) in getCountries" :key="index">
+        <li>
+           Country: {{item}} : Confirmed cases {{item.confirmedCases}}
+        </li>
+    </ul>
 </div>
 </template>
 
@@ -10,20 +14,36 @@ export default {
     name: 'Home',
     data(){
         return {
-            values : {},
-            error : []
+            countries : {},
+            errors : []
         }
     },
     mounted(){
-        axios.get('https://apicovid19.com/summary')
+        axios.get('https://api.covid19api.com/summary')
         .then( response => {
-            this.values = response.data
+            this.countries = response.data;
+        }).catch( e => {
+            this.errors.push(e)
         })
     },
     methods: {
         test(){
             console.log(this.values)
             alert('Testing')
+        }
+    },
+    computed:{
+        getCountries(){
+            let details = [];
+
+            this.countries.Countries.forEach( country => {
+                details.push({
+                    countryName: country.Country,
+                    confirmedCases: country.TotalConfirmed
+                })
+            })
+            return details.filter(x => x.confirmedCases > 0 && x.countryName.length >0)
+            .sort(x => x.confirmedCases);
         }
     }
 }
