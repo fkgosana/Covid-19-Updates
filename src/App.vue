e8c475204abaa711d2a3f33874efbb8cb6f9bc77
Frans <fkgosana48@gmail.com>
2020-04-01 17:53:41 +0200

    add stats components

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,66 @@
 <template>
   <div id="app">
-    <Home/>
+    <section name="info" id="info">
+      <h1>Dintlafatso tsa Corona virus</h1>
+      <p>
+        Dikokwanatlhoko tsa Corona ke lefapha la dikokwanatlhoko tse di ka tlholang golwala mo diphoofolong
+        gammogo le go batho. Mo bathong, di tsebega ka go tlhola Respiratory Infection tseo e kabang go tonya
+        le go thotomela gammogo le malwetsi a go swana le Middle East Respiratory Syndrome (SARS). Dikokwanatlhoko
+        tsa Corona tseo di utullotswego ga bjale ke kokwanatlhoko ya bolwetsi eo e bitswago gore ke COVID-19
+      </p>
+      <section class="stats">
+        <statistics :title="confirmed" :numbers="totalConfirmedCases"/>
+        <statistics :title="recovered" :numbers="totalRecovered"/>
+        <statistics :title="deaths" :numbers="totalDeath"/>
+      </section>
+    </section>
+    <section class="updates">
+      <Updates :countries="update"/>
+    </section>
   </div>
 </template>
 
 <script>
-import Home from './components/Home.vue';
+import Updates from './components/Updates.vue';
+import statistics from './components/statistics';
+import covidUpdates from './covid19updates.json';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      deaths : "Total Death",
+      recovered : "Total Recovered",
+      confirmed : "Total Confirmed Cases",
+      update : covidUpdates,
+    }
+  },
   components: {
-    Home
+    Updates,
+    statistics
+  },
+  computed:{
+    totalDeath(){
+      let results = 0;
+      this.update.Countries.forEach( country => {
+        results += country.TotalDeaths
+      })
+      return results;
+    },
+    totalConfirmedCases(){
+      let results = 0;
+      this.update.Countries.forEach( country => {
+        results += country.TotalConfirmed;
+      })
+      return results;
+    },
+    totalRecovered(){
+      let results = 0;
+      this.update.Countries.forEach( country => {
+        results += country.TotalRecovered;
+      })
+      return results;
+    }
   }
 }
 </script>
@@ -23,5 +73,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  color: greenyellow
+}
+.updates {
+  text-align : center;
+}
+.stats {
+  display: inline-block;
 }
 </style>
---
+++ src/components/statistics.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{title}}</h5>
+            <p>{{numbers}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'statistics',
+    props : {
+        title: String,
+        numbers: Number
+    }
+}
+</script>
